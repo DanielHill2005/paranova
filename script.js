3b5a574c40a3037b5d34f6f08b04ec3b1088f5ee
DanielHill2005 <93940164+DanielHill2005@users.noreply.github.com>
2022-04-22 09:43:06 -0700

    commented and trimmed

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-var timeLeft = 30;
+var timeLeft = 30; //defines all the variables
 var oldMan = 0;
-const story = {
+const story = { //holds the entire story
     one: 
     {
-        text: `tavern `,
-        oldManText: '', 
-        variable: 0,               
+        text: `tavern `,  //main text
+        oldManText: '',  //optional text
+        variable: 0, //holds what variables will be changed         
         choices: [
             [`two`,`outside`],
             [`three`,`dungeon`],
             [`four`,`castle`]
-        ],
-        time: 0
+        ], //holds all the choices
+        time: 0 //holds how much time is subtracted to get to this choice
     },
     two: 
     {
@@ -43,37 +43,30 @@ const story = {
         time: 3
     }
 };
-let choiceList = document.getElementById('choices'), choices = [];
 
-// const newButton = document.createElement("button");
-function storyLoop (number){
-    alert(timeLeft);
-    document.getElementById('choiceMenu').innerHTML = '';
-    let currentAddedText = '';
-    if(oldMan == 0){
+function storyLoop (number){ //all of the mechanics
+    timeLeft -= story[number].time; //subtracts time 
+    for(i = 0; i < story[number].variable.length; i+=2){ //updates all variables
+        switch (story[number].variable[i]){ //uses the first value in a set to point to which variable to update
+            case 'oldMan': 
+                oldMan += story[number].variable[i+1]; //updates variable by the second value in a set
+                break;
+            default:
+                alert('something broke lol, try restarting'); //debugging tool
+        }
+    }    
+    document.getElementById('choiceMenu').innerHTML = ''; //clears all of the old choices
+    let currentAddedText = ''; //holds the new text to be added
+    if(oldMan == 0){ //adds the main text and optional text if needed
         currentAddedText = story[number].text;
     } else if (oldMan == 1) {
     currentAddedText = story[number].text + story[number].oldManText;
     }
-    document.getElementById('storyText').textContent += `\n ${currentAddedText}`;
-    for(i = 0; i < story[number].choices.length; i++){
-        let btn = document.createElement('button');
-        btn.setAttribute('onclick', `varUpdater('${story[number].choices[i][0]}'), storyLoop('${story[number].choices[i][0]}')`);
-        btn.innerHTML = story[number].choices[i][1];
-        document.getElementById('choiceMenu').appendChild(btn);
-    }
-    
-}
-
-function varUpdater (number) {
-    timeLeft -= story[number].time;
-    for(i = 0; i < story[number].variable.length; i+=2){
-        switch (story[number].variable[i]){
-            case 'oldMan':
-                oldMan += story[number].variable[i+1];
-                break;
-            default:
-                alert('something broke lol');
-        }
-    }
+    document.getElementById('storyText').textContent += `\n ${currentAddedText}`; //adds the text to the main story
+    for(i = 0; i < story[number].choices.length; i++){ //loops for how many choices there are
+        let btn = document.createElement('button'); //creates a button HTML tag inside of a variable
+        btn.setAttribute('onclick', `storyLoop('${story[number].choices[i][0]}')`); //adds an onclick attribute to the button with the specific pointer needed
+        btn.innerHTML = story[number].choices[i][1]; //adds the button text
+        document.getElementById('choiceMenu').appendChild(btn); //adds the button to the UI
+    }   
 }
